ca90313b7f6e48093f2007e999ab6e3016c09ee9
Urbano Ballesteros <urbano@cbta193.edu.mx>
2020-12-31 00:53:00 -0600

    Create label component

---
+++ projects/uulala-components/src/lib/uul-label/uul-label.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-uul-label',
+  templateUrl: './uul-label.component.html',
+  styleUrls: ['./uul-label.component.scss']
+})
+export class UulLabelComponent implements OnInit {
+  //control config
+  @Input() tittle:string = '';
+  @Input() description:string = '';
+
+  //Input styles
+  @Input() containerCss: string = '';
+  @Input() tittleCss: string = '';
+  @Input() descriptionCss: string = '';
+
+  //local variables
+  containerStyles:any = {
+    'label-container': true
+  };
+
+  tittleStyles:any = {
+    'tittle-label': true
+  }
+  descriptionStyles:any = {
+    'description-label': true
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.loadStyles();
+  }
+
+  loadStyles() {
+    this.containerStyles[this.containerCss] = this.containerCss != '';
+    this.tittleStyles[this.tittleCss] = this.tittleCss != '';
+    this.descriptionStyles[this.descriptionCss] = this.descriptionCss != '';
+  }
+
+}
--- projects/uulala-components/src/lib/uulala-components.module.ts
+++ projects/uulala-components/src/lib/uulala-components.module.ts
@@ -4,15 +4,16 @@ import { UulInputTextComponent } from './input-text/uul-input-text.component';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveFormsModule } from '@angular/forms';
+import { UulLabelComponent } from './uul-label/uul-label.component';
 
 
 
 @NgModule({
-  declarations: [UulalaComponentsComponent, UulInputTextComponent],
+  declarations: [UulalaComponentsComponent, UulInputTextComponent, UulLabelComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule
   ],
-  exports: [UulalaComponentsComponent, UulInputTextComponent]
+  exports: [UulalaComponentsComponent, UulInputTextComponent, UulLabelComponent]
 })
 export class UulalaComponentsModule { }
