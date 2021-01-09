e2f5efff390d7006b6b5315a666e31572b22710f
Urbano Ballesteros <urbano@cbta193.edu.mx>
2021-01-08 18:25:21 -0600

    add check component

---
+++ projects/uulala-components/src/lib/uul-check/uul-check.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-uul-check',
+  templateUrl: './uul-check.component.html',
+  styleUrls: ['./uul-check.component.scss']
+})
+export class UulCheckComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- projects/uulala-components/src/lib/uulala-components.module.ts
+++ projects/uulala-components/src/lib/uulala-components.module.ts
@@ -7,7 +7,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { UulLabelComponent } from './uul-label/uul-label.component';
 import { UulSelectComponent } from './uul-select/uul-select.component';
 import { StepsComponent} from './steps/uul-steps.component';
-import { UulImgComponent } from './uul-img/uul-img.component'
+import { UulImgComponent } from './uul-img/uul-img.component';
+import { UulCheckComponent } from './uul-check/uul-check.component'
 
 
 
@@ -18,7 +19,8 @@ import { UulImgComponent } from './uul-img/uul-img.component'
     UulLabelComponent,
     UulSelectComponent,
     StepsComponent,
-    UulImgComponent
+    UulImgComponent,
+    UulCheckComponent
   ],
   imports: [
     CommonModule,
