4eafcfdc61cfa59d51fd6f069e97a969777f3664
Urbano Ballesteros <urbano@cbta193.edu.mx>
2021-01-16 00:43:21 -0600

    Add functionalities and fix errors

--- projects/uulala-components/src/lib/utilities/string.tools.ts
+++ projects/uulala-components/src/lib/utilities/string.tools.ts
@@ -3,7 +3,7 @@ import { NumberTools } from './number.tools';
 
 export class StringTools {
 
-  static generateNewRandomString(length: number) {
+  static generateNewRandomString(length: number) : string {
     let result: string = '';
     let characters: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let charactersLength: number = characters.length;
@@ -13,12 +13,29 @@ export class StringTools {
     return result;
   }
 
-  static generateNewIdString() {
+  static generateNewIdString() : string {
     let length = NumberTools.generateRandomNumber(NumberTools.generateRandomNumber(100000))
     return StringTools.generateNewRandomString(length);
   }
 
-  static generateNewHashString() {
+  static generateNewHashString() : string {
     return Md5.hashStr(StringTools.generateNewIdString()).toString()
+  }
+
+  static getGraphErrorMessage(error: string) : string{
+    let errorString: string = error.replace('GraphQL.ExecutionError: ', '');
+    errorString = errorString.substring(0, errorString.length - 1);
+
+    let errors: string[] = errorString.split('|');
+
+    if(errors.length !== 0) return errors[0];
+    else return '';
+  }
+
+  static getGraphErrorMessageList(error: string) : string[] {
+    let errorString: string = error.replace('GraphQL.ExecutionError: ', '');
+    errorString = errorString.substring(0, errorString.length - 1);
+
+    return errorString.split('|');
   }
 }
