1eeff64cf532be9d84923cb1632bd35b84a7423e
Guadalupe Torres <guadalupe@uulala.io>
2021-05-06 17:13:32 -0500

    Update uul-img-background.component.ts

--- projects/uulala-components/src/lib/uul-img-background/uul-img-background.component.ts
+++ projects/uulala-components/src/lib/uul-img-background/uul-img-background.component.ts
@@ -1,6 +1,5 @@
 import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { InputControlImgConfig } from '../models/configurations';
-import { ImageTypes } from '../models/types';
 import { StringTools } from '../utilities';
 @Component({
   selector: 'uul-img-background',
@@ -15,21 +14,14 @@ export class UulImgBackgroundComponent implements OnInit, AfterViewInit {
   };
   @Input() url: string = '';
   @Input() children: string = '';
-  @Input() containerCss: string = '';
-  @Input() imageCss: string = '';
-  @Input() type: ImageTypes;
   @Input() imgWidth: string = '280';
   @Input() imgHeight: string = '120';
-
-  containerElement: HTMLElement;
   imgElement: HTMLElement;
 
   constructor() { }
 
   ngOnInit(): void {
-      console.log('on init image ', this.type, this.url)
-      this.loadStyles();
-
+ 
   }
 
   ngAfterViewInit() {
@@ -38,36 +30,9 @@ export class UulImgBackgroundComponent implements OnInit, AfterViewInit {
   }
 
   loadElements() {
-    this.containerElement = document.getElementById(`${this.config.id}-container`);
     this.imgElement = document.getElementById(`${this.config.id}-img`);
   }
 
-  loadStyles() {
-    if (this.containerCss != '') {
-      this.containerCss.split(' ').forEach(cssClass => {
-        this.containerElement.classList.add(cssClass);
-      });
-    }
-    if (this.imageCss != '') {
-      this.imageCss.split(' ').forEach(cssClass => {
-        this.imgElement.classList.add(cssClass);
-      });
-    }
-
-    // if (this.url != '' && this.url !== null) this.imgElement.style.backgroundImage = `url("${this.url}")`;
-  }
-
-  reloadImg(url: string, children: string = '') {
-    if(this.imgElement) {
-      this.loadElements();
-      this.url = url;
-      if (this.url != '' && this.url !== null) this.imgElement.style.backgroundImage = `url("${this.url}")`;
-    }
-    else {
-      this.url = '';
-      this.children = children
-    }
-  }
 
   getSideNavBarStyle() {
     let imageBgStyle: any = {};
