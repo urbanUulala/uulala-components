96a51ff77844dad35f15a367308747469f7232ed
Urbano Ballesteros Rodriguez <urbano@uulala.io>
2021-07-15 14:46:38 -0500

    More colors to batched and optional property brand

--- projects/uulala-components/src/lib/models/getters/brand.model.ts
+++ projects/uulala-components/src/lib/models/getters/brand.model.ts
@@ -3,6 +3,6 @@ import { BrandInfoModel } from "./brandinfo.model";
 export interface BrandModel {
     id: string;
     name: string;
-    brandeo: BrandInfoModel
+    brandeo?: BrandInfoModel
     totalCards: number;
 }
