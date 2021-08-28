90bb70c6ace97f727f5a40f5aaca6cdf8f4b4eb8
Urbano Ballesteros Rodriguez <urbano@uulala.io>
2021-08-27 20:38:12 -0500

    Add references to constants

--- projects/uulala-components/src/lib/constants/index.ts
+++ projects/uulala-components/src/lib/constants/index.ts
@@ -1,3 +1,5 @@
 export * from './auth.constants';
 export * from './licences.constants';
 export * from './metadata.constants';
+export * from './licences.constants';
+export * from './points.constants';
