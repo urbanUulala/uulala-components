b3952e76ce49d1c429dc115f009c9b98db74a0e7
victor-uulala <victor@uulala.io>
2022-02-09 22:48:49 -0600

    Update configurations.ts

--- projects/uulala-components/src/lib/models/configurations.ts
+++ projects/uulala-components/src/lib/models/configurations.ts
@@ -8,6 +8,7 @@ export interface InputControlImgConfig extends InputControlConfig{}
 
 // Structures for select component
 export interface SelectListItem {
+  icon?: string,
   value: string,
   description: string,
   short_name?: string
